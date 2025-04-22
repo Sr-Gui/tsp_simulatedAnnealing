9c0014bcd7321131b62bd83d194731ae58d4015d
Guilherme Souza Rocha <guilherme.souzarocha.ismart@gmail.com>
2025-04-21 23:14:05 -0300

    2

--- simulatedAnnealing/simulatedAnnealing.py
+++ simulatedAnnealing/simulatedAnnealing.py
@@ -1,27 +1,49 @@
 import numpy as np
 
 #TSP
-sol=()
+
+#Teste tsp fake
+tsp=np.array([
+  [0,1],
+  [4,5],
+  [8,9],
+  [1,0]
+])
+n=len(tsp)
+
+def dist(a,b):
+  return np.sqrt((np.sum((a-b)**2))) #distancia pseudo-euclidiana? ATT
 
 def cost(sol):
-  x,y,z=sol
-  c=()
+  c=0
+  for i in range(n-1):
+    c+=dist(tsp[sol[i]],tsp[sol[i+1]])
+  c+=dist(tsp[sol[-1]],tsp[sol[0]])
   return c
 
 def tweak(s):
-
-  return 0
+  r=s.copy()
+  i,j=np.random.choice(n,2)
+  r[i],r[j]=r[j],r[i]
+  return r
 
 #Parametros
-T=1000
-S = ()
-S=max(sol)
-Best=S
-while():
-  R=tweak(S.copy())
-  n=np.random()
-  if (cost(R)>cost(S)) or ():
+t=1000
+min_t=0.001
+a=0.9
+
+#sol inicial
+S=np.arange(n)
+best=S.copy()
+
+while(t>min_t):
+  R=tweak(S)
+  d=cost(R)-cost(S)
+  if (d>0) or (np.random.rand()<np.exp(d/t)):
     S=R
-    T=T-1
-  if cost(S) > cost(Best):
-    Best=S
+    if d<0:
+      best=S
+  t*=a #resfriamento/decrease
+
+#teste
+  print(best,cost(best),t)
