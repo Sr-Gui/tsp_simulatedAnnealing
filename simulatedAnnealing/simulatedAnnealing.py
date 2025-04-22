c44e611252e743e50da25d28c5d41a427a76a361
Guilherme Souza Rocha <guilherme.souzarocha.ismart@gmail.com>
2025-04-22 00:39:49 -0300

    3

--- simulatedAnnealing/simulatedAnnealing.py
+++ simulatedAnnealing/simulatedAnnealing.py
@@ -1,8 +1,31 @@
 import numpy as np
+import tsplib95
 
 #TSP
+tsp=tsplib95.load('../TSP/att48.tsp')
+node=list(tsp.get_nodes())
+n=len(node)
 
-#Teste tsp fake
+dist=np.zeros((n,n),dtype=int)
+for i in range(n):
+  for j in range(n):
+    if i!=j:
+      dist[i][j]=tsp.get_weight(i+1,j+1)
+
+def cost(sol):
+  c=0
+  for i in range(n-1):
+    c+=dist[sol[i]][sol[(i+1)]]
+  return c
+
+#2-opt
+def tweak1(sol):
+  i,j=sorted(np.random.choice(n,2,replace=False))
+  R=sol.copy()
+  R[i:j+1]=sol[i:j+1][::-1]
+  return R
+
+""" #Teste tsp fake
 tsp=np.array([
   [0,1],
   [4,5],
@@ -12,7 +35,7 @@ tsp=np.array([
 n=len(tsp)
 
 def dist(a,b):
-  return np.sqrt((np.sum((a-b)**2))) #distancia pseudo-euclidiana? ATT
+  return np.sqrt((np.sum((a-b)**2)))
 
 def cost(sol):
   c=0
@@ -25,7 +48,7 @@ def tweak(s):
   r=s.copy()
   i,j=np.random.choice(n,2)
   r[i],r[j]=r[j],r[i]
-  return r
+  return r """
 
 #Parametros
 t=1000
@@ -37,7 +60,7 @@ S=np.arange(n)
 best=S.copy()
 
 while(t>min_t):
-  R=tweak(S)
+  R=tweak1(S)
   d=cost(R)-cost(S)
   if (d>0) or (np.random.rand()<np.exp(d/t)):
     S=R
