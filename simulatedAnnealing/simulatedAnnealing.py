18bec2de09553991bc0b3ac62eb2ce1b81f6aa0f
Guilherme Souza Rocha <guilherme.souzarocha.ismart@gmail.com>
2025-04-21 21:44:42 -0300

    1

---
+++ simulatedAnnealing/simulatedAnnealing.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+#TSP
+sol=()
+
+def cost(sol):
+  x,y,z=sol
+  c=()
+  return c
+
+def tweak(s):
+
+  return 0
+
+#Parametros
+T=1000
+S = ()
+S=max(sol)
+Best=S
+while():
+  R=tweak(S.copy())
+  n=np.random()
+  if (cost(R)>cost(S)) or ():
+    S=R
+    T=T-1
+  if cost(S) > cost(Best):
+    Best=S
