834ef137b370e24eaa99485601da59e371098cec
Guilherme Souza Rocha <guilherme.souzarocha.ismart@gmail.com>
2025-04-24 01:53:46 -0300

    4

--- simulatedAnnealing/simulatedAnnealing.py
+++ simulatedAnnealing/simulatedAnnealing.py
@@ -1,5 +1,8 @@
 import numpy as np
 import tsplib95
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
 
 #TSP
 tsp=tsplib95.load('../TSP/att48.tsp')
@@ -16,57 +19,62 @@ def cost(sol):
   c=0
   for i in range(n-1):
     c+=dist[sol[i]][sol[(i+1)]]
+  c += dist[sol[-1]][sol[0]]
   return c
 
 #2-opt
-def tweak1(sol):
+def tweak(sol):
   i,j=sorted(np.random.choice(n,2,replace=False))
   R=sol.copy()
   R[i:j+1]=sol[i:j+1][::-1]
   return R
 
-""" #Teste tsp fake
-tsp=np.array([
-  [0,1],
-  [4,5],
-  [8,9],
-  [1,0]
-])
-n=len(tsp)
+def simulatedAnnealing(t0=1000,t1=0.001,a=0.9):
 
-def dist(a,b):
-  return np.sqrt((np.sum((a-b)**2)))
+  S=np.arange(n)
+  best=S.copy()
+  
+  T=t0
+  i=0
+  start=time.time()
 
-def cost(sol):
-  c=0
-  for i in range(n-1):
-    c+=dist(tsp[sol[i]],tsp[sol[i+1]])
-  c+=dist(tsp[sol[-1]],tsp[sol[0]])
-  return c
+  while(t0>t1):
+    R=tweak(S)
+    d=cost(R)-cost(S)
+    if (d<0) or (np.random.rand()<np.exp(-d/t0)):
+      S=R
+      if cost(S)<cost(best):
+        best=S
+    t0*=a #resfriamento/decrease
+    i+=1
+    # print(best,cost(best),t)
+    end=time.time()
+    time_f=end-start
+  return best,T,t1,a,i,time_f
 
-def tweak(s):
-  r=s.copy()
-  i,j=np.random.choice(n,2)
-  r[i],r[j]=r[j],r[i]
-  return r """
+#plot
+def results(best,c,t0,t1,a,i,time_f):
+  df=pd.DataFrame({
+    'Parametros':['T0','T1','a','i','Custo','Tempo'],
+    'Value':[t0,t1,a,i,c,time_f]
+    })
+  print('Resultados')
+  print('Melhor caminho: ',best)
+  print(df.to_markdown(index=False))
 
-#Parametros
-t=1000
-min_t=0.001
-a=0.9
+def res_rota(tsp,sol):
+  coord=tsp.node_coords
+  x=[coord[i+1][0] for i in sol]+[coord[sol[0]+1][0]]
+  y=[coord[i+1][1] for i in sol]+[coord[sol[0]+1][1]]
 
-#sol inicial
-S=np.arange(n)
-best=S.copy()
+  plt.figure()
+  plt.plot(x,y)
+  for i,(xi,yi) in enumerate(zip(x,y)):
+    plt.text(xi,yi,str(sol[i%len(sol)]+1),fontsize=8)
+  plt.grid(True)
+  plt.show()
 
-while(t>min_t):
-  R=tweak1(S)
-  d=cost(R)-cost(S)
-  if (d>0) or (np.random.rand()<np.exp(d/t)):
-    S=R
-    if d<0:
-      best=S
-  t*=a #resfriamento/decrease
+best,t0,t1,a,i,time_f = simulatedAnnealing()
 
-#teste
-  print(best,cost(best),t)
+results(best,cost(best),t0,t1,a,i,time_f)
+res_rota(tsp,best)
